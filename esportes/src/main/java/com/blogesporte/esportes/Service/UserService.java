7ea1d02861f0e53f985bf55566ee71998aa3854d
felipe.both <felipe.sfreitas14@gmail.com>
2024-01-18 15:53:37 -0300

    Projeto rodando mas ainda sem testar

--- esportes/src/main/java/com/blogesporte/esportes/Service/UserService.java
+++ esportes/src/main/java/com/blogesporte/esportes/Service/UserService.java
@@ -1,13 +1,14 @@
 package com.blogesporte.esportes.Service;
 
-import org.apache.catalina.User;
+
+import com.blogesporte.esportes.model.User;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public interface UserService {
     Optional<com.blogesporte.esportes.model.User> findById (Long id);
     User saveUser(User user);
     void deleteUser(Long id);
-
-    com.blogesporte.esportes.model.User saveUser(com.blogesporte.esportes.model.User user);
 }
--- esportes/src/main/java/com/blogesporte/esportes/Service/UserServiceImpl.java
+++ esportes/src/main/java/com/blogesporte/esportes/Service/UserServiceImpl.java
@@ -3,30 +3,45 @@ package com.blogesporte.esportes.Service;
 import com.blogesporte.esportes.Repository.UserRepository;
 import com.blogesporte.esportes.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
-public abstract class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository){
+    public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
     @Override
     @Transactional(readOnly = true)
-    public Optional<User> findById(Long id){
-        return Optional.ofNullable(userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("User not find ")));
-
+    public Optional<User> findById(Long id) {
+        return Optional.ofNullable(userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("User not found with id: " + id)));
     }
+
     @Override
     @Transactional
-    public void deleteUser(Long id){
-        Optional<User> user =  findById(id);
+    public User saveUser(User user) {
+        return userRepository.save(user);
     }
 
+    @Override
+    @Transactional
+    public void deleteUser(Long id) {
+        User user = findById(id)
+                .orElseThrow(() -> new UserNotFoundException("User not found with id: " + id));
+        userRepository.delete(user);
+    }
+
+    // Defina a exceção UserNotFoundException
+    private static class UserNotFoundException extends RuntimeException {
+        public UserNotFoundException(String message) {
+            super(message);
+        }
+    }
 }
